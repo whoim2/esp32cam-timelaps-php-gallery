402df7940b478acf73170d6b57ad4488a1e20f27
whoim2 <whoim@mail.ru>
2021-06-06 03:53:18 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 /*******************************************
 php image timelaps gallery for 3d-printing
 based on esp32cam
-https://github.com/whoim2/esp32cam-timelaps-php-gallery
+
 ********************************************/
 //settings
 date_default_timezone_set('Europe/Moscow');
@@ -19,6 +19,7 @@ if(isset($_GET['new']) || scandir($folder, SCANDIR_SORT_DESCENDING)[0] == '..') 
   }
 }
 $f = $folder.scandir($folder, SCANDIR_SORT_DESCENDING)[0]."/";
+if(file_exists($f.'README')) unlink($f.'README');
 $received = file_get_contents('php://input'); //try to get input stream
 $size = strlen($received);
 if($size > 0 && $_SERVER["CONTENT_TYPE"] == 'image/jpg') { //if stream not empty and this jpeg image
@@ -52,7 +53,9 @@ $folders = scandir($folder, SCANDIR_SORT_DESCENDING);
 foreach($folders as $dir)
   if($dir <> '.' && $dir <> '..' && $dir <> 'README') {
       $files = scandir($folder.'/'.$dir, SCANDIR_SORT_ASCENDING);
-      $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-      print '<a href="'.$link.'?download='.$dir.'"><img src="'.$link.$data_folder.$dir.'/'.$files[2].'" title="'.date("d.m.Y H:i:s", $dir).'"  width="100" /></a>&nbsp;';
+      if(sizeof($files) > 2) {
+        $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        print '<a href="'.$link.'?download='.$dir.'"><img src="'.$link.$data_folder.$dir.'/'.$files[2].'" title="'.date("d.m.Y H:i:s", $dir).'"  width="100" /></a>&nbsp;';
+      }
 }
 ?>
